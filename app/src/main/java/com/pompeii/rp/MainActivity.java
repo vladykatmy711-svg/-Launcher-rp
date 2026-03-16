6cc5f0a8ca100157eeb0e782ce3ad3ce742dd0ee
vladykatmy711-svg <vladykatmy711@gmail.com>
2026-03-16 22:45:28 +0500

    Update MainActivity.java

--- app/src/main/java/com/pompeii/rp/MainActivity.java
+++ app/src/main/java/com/pompeii/rp/MainActivity.java
@@ -3,12 +3,12 @@ package com.pompeii.rp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.byreytiz.game.CrashHandler;
+import com.byreytiz.game.R;  // Добавь эту строку
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
         
         Thread.setDefaultUncaughtExceptionHandler(new CrashHandler());
         
