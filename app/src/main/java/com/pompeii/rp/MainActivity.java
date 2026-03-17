df35be7a4630f3a7fe0035824838f43f3247f04c
vladykatmy711-svg <vladykatmy711@gmail.com>
2026-03-17 07:39:49 +0500

    Update MainActivity.java

--- app/src/main/java/com/pompeii/rp/MainActivity.java
+++ app/src/main/java/com/pompeii/rp/MainActivity.java
@@ -10,6 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
+        // Подавляем ошибку OnBackInvokedCallback
+        if (android.os.Build.VERSION.SDK_INT >= 34) {
+            getOnBackInvokedDispatcher().registerOnBackInvokedCallback(
+                android.window.OnBackInvokedDispatcher.PRIORITY_DEFAULT,
+                () -> {}
+            );
+        }
+        
         try {
             setContentView(R.layout.activity_main);
             Toast.makeText(this, "Активити загружена", Toast.LENGTH_SHORT).show();
