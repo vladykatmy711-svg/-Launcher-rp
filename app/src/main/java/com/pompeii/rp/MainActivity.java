93949fb3b543e94cc36f7b535204e33ed1a91b1e
vladykatmy711-svg <vladykatmy711@gmail.com>
2026-03-17 10:41:35 +0500

    Fix MainActivity - complete

--- app/src/main/java/com/pompeii/rp/MainActivity.java
+++ app/src/main/java/com/pompeii/rp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
+import com.pompeii.rp.R;
 
 public class MainActivity extends AppCompatActivity {
     @Override
